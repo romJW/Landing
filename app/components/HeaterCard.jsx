5d74f1984541da8425d6ee19dc69d62dcc70f2ee
Roman Bukharin <buxarin1999@gmail.com>
2022-08-23 22:49:45 +0600

    add heater

--- app/components/HeaterCard.jsx
+++ app/components/HeaterCard.jsx
@@ -3,7 +3,7 @@ export default function (props) {
         <div className="card rounded-sm shadow-[0_0_40px_rgba(0,0,0,0.08)] w-full basis-1/4 md:basis-1/3 lg:basis-1/4 xl:basis-1/6 bg-white rounded-lg">
         <div className="card-body">
           <div className="image mx-auto">
-            <img src={props.card.pic} alt=""/>
+            <img src={props.card.pic} alt="" className="w-9 h-9 sm:w-11 sm:h-11"/>
           </div>
           <div className="text text-center tracking-tight text-lg">
             <p><pre className="text-sm sm:text-lg" style={{ fontFamily: 'inter-regular, sans-serif',  lineHeight: '1.4' }}>{props.card.text}</pre></p>
