be94dc5c8a61bd9ee293c909aa65bb97ff81fb8d
Elisey Sharipov <se660745@gmail.com>
2022-08-25 18:38:59 +0000

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,7 +2,7 @@ export default function(props){
   return(
     <div className="outline outline-green-600 py-5">
       <div className="w-5/6 py-5 pr-10 m-auto">
-        <img src="images/abbex.png" alt="" className="mb-5"/>
+        <img src="assets/abbex.png" alt="" className="mb-5"/>
         <footer className="footer bg-neutral text-neutral-focus inline-grid grid-cols-2 gap-y-2 gap-x-12 md:gap-x-14 md:grid md:grid-flow-column md:gap-10 md:grid-cols-4 relative">
           <div>
             <span className="footer-title footer-headlines opacity-100">Информация</span> 
@@ -22,9 +22,9 @@ export default function(props){
             <a className="link-hover font underline underline-offset-4">Получить обратный звонок</a>
           </div>
           <div className="grid grid-flow-col gap-4 mt-5 float-right w-32 xs:mt-20 md:mt-5 md:absolute right-0 top-12">
-            <a><img src="images/social1.png" alt=""/></a>
-            <a><img src="images/social2.png" alt="" /></a>
-            <a><img src="images/social3.png" alt="" /></a>
+            <a><img src="assets/social1.png" alt=""/></a>
+            <a><img src="assets/social2.png" alt="" /></a>
+            <a><img src="assets/social3.png" alt="" /></a>
           </div>
           <button className="btn btn-primary btn-md block w-52 right-0 my-4 md:text-xs md:w-fit md:my-0 md:absolute lg:text-sm lg:w-52">Оставить заявку</button>
         </footer>
