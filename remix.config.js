d3999b2b11dbf2a726f04104b19d4d06fc7c1d82
Drake Nelson <drake.aren@pm.me>
2022-08-30 02:50:47 +0600

    Add deploy to netlify

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "netlify/functions/server/build",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
 };
